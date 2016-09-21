217301c8939c82939040c6f8d8e068ed1e9c6e70
James <jsunthon@gmail.com>
2016-09-21 11:17:04 -0700

    Added code for computing area of graph using trapezoidal rule of parallel prog

--- practice/mpi_hello.c
+++ practice/mpi_hello.c
@@ -8,11 +8,11 @@ int main(void) {
     char greeting[MAX_STRING];
     int comm_sz;
     int my_rank;
-    
+
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    
+
     if (my_rank != 0) {
         sprintf(greeting, "Greetings from process %d of %d", my_rank, comm_sz);
         MPI_Send(greeting, strlen(greeting) + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
@@ -24,7 +24,7 @@ int main(void) {
             printf("%s\n", greeting);
         }
     }
-    
+
     MPI_Finalize();
     return 0;
 }
